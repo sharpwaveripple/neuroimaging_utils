0aadf54c9ff1ad9df75389b6405a062fcff3536f
sharpwaveripple <jonathantay193@gmail.com>
2018-04-29 00:54:59 +0100

    removed implicit addition of inverse xfm

--- BBR_fmapless.sh
+++ BBR_fmapless.sh
@@ -4,7 +4,8 @@
 # INPUT2: Raw anat
 # INPUT3: Brain extracted anat
 # INPUT4: White matter seg
-# INPUT5: Output basename
+# INPUT5: Output matrix
+# INPUT6: Inverse output matrix
 
 # OUTPUT: BBR transform and its inverse
 
@@ -13,13 +14,12 @@ t1=$2
 t1brain=$3
 wmseg=$4
 out=$5
+out_inv=$6
 
-out_file=$(basename $out .nii.gz)
+out_file=$(basename $out .mat)
 out_dir=$(dirname $out) 
 expected_wmseg=$out_dir/${out_file}_fast_wmseg.nii.gz
 wmedge=$out_dir/${out_file}_fast_wmedge.nii.gz
-bbr_xfm=$out_dir/${out_file}.mat
-inv_bbr_xfm=$out_dir/${out_file}_inv.mat
 
 if [ $wmseg != $expected_wmseg ]; then
   echo "$wmseg doesn't follow FSL convention, copying as $expected_wmseg"
@@ -28,8 +28,8 @@ fi
 
 epi_reg --epi=$epi --t1=$t1 --t1brain=$t1brain --out=$out --wmseg=$wmseg -v
 
-echo "Inverting $bbr_xfm..."
-convert_xfm -omat $inv_bbr_xfm -inverse $bbr_xfm
+echo "Saving inverse of $out as $out_inv..."
+convert_xfm -omat $out_inv -inverse $out
 
 echo "Removing redunant files..."
 rm ${out}.nii.gz $out_dir/${out_file}_init.mat
