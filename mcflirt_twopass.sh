54772f9ffcf8cb86e49b7907bf2872b238f7942b
sharpwaveripple <jonathantay193@gmail.com>
2018-04-28 20:41:09 +0100

    added some documentation for twopass

--- mcflirt_twopass.sh
+++ mcflirt_twopass.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-# NOTE: Because it's MCFLIRT, OMIT FILE EXTENSIONS!!
+# INPUT: Raw functional to be motion corrected
+# OUTPUT: Two pass motion corrected file in the same folder
 
-raw_func=$1
+func=$1
 
-echo "Calculating first pass with $raw_func..."
+func_file=$(basename $func .nii.gz)
+func_dir=$(dirname $func) 
+mcf_func=$func_dir/${func_file}_mcf
+first_pass=$func_dir/first_pass 
+first_pass_mean=$func_dir/first_pass_mean
 
-mcflirt -in $raw_func \
-  -out first_pass \
+echo "Calculating first pass with $func..."
+
+mcflirt -in $func \
+  -out $first_pass \
   -meanvol \
   -spline_final \
   -report
 
-fslmaths first_pass -Tmean first_pass_mean
+fslmaths $first_pass -Tmean $first_pass_mean
 
-echo "Calculating second pass with $raw_func..."
+echo "Calculating second pass with $func_file..."
 
-mcflirt -in $raw_func \
-  -reffile first_pass_mean \
+mcflirt -in $func \
+  -out $mcf_func \
+  -reffile $first_pass_mean \
   -stages 4 \
   -spline_final \
   -plots \
   -report
 
-fslmaths ${raw_func}_mcf.nii.gz -Tmean ${raw_func}_mcf_mean.nii.gz
+echo "Saved $mcf_func"
 
-rm first_pass.nii.gz first_pass_mean.nii.gz
+rm ${first_pass}.nii.gz ${first_pass_mean}.nii.gz 
