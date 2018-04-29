d8e60c32b757a431900f272a5020e53b4f485f9b
sharpwaveripple <jonathantay193@gmail.com>
2018-04-29 22:55:54 +0100

    added removval script

--- TPM_brain_extraction.sh
+++ TPM_brain_extraction.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# INPUT1: Whole head T1
+# INPUT2: Brain extracted T1
+# INPUT3: T1 brain mask
+# INPUT4: TPM1
+# INPUT5: TPM2
+# INPUT6: TPM3
+# OUTPUT: Brain extracted T1
+
 T1=$1
-TPM1=$2
-TPM2=$3
-TPM3=$4
-T1_brain=$5
+T1_brain=$2
+T1_mask=$3
+TPM1=$4
+TPM2=$5
+TPM3=$6
 
-fslmaths $TPM1 -add $TPM2 -add $TPM3 -bin bin_brain.nii.gz
-fslmaths $T1 -mas bin_brain.nii.gz $T1_brain
-rm bin_brain.nii.gz 
-
-N4BiasFieldCorrection \
-  -d 3 \
-  -v \
-  -i $T1_brain \
-  -o $T1_brain
+fslmaths $TPM1 -add $TPM2 -add $TPM3 -bin $T1_mask
+fslmaths $T1 -mas $T1_mask $T1_brain
+rm $T1_mask
